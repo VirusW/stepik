265935206ef0d1c6719ceec3d174f6ec51bb0acb
alexandr.bezpalyi@gmail.com <alexandr.bezpalyi@gmail.com>
2020-09-20 12:11:01 +0300

    add implicit waits

--- test_3-6.py
+++ test_3-6.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 import pytest
 
 import time
@@ -17,35 +21,29 @@ links = [
 
 class TestLesson:
 
-    @pytest.fixture
-    def browser(self):
-        drv = webdriver.Chrome()
-        drv.implicitly_wait(10)
-        drv.get('https://stepik.org/catalog?language=en')
-        login = drv.find_element_by_link_text("Log in")
-        login.click()
-
-        name = drv.find_element_by_css_selector('input[name="login"]')
-        name.send_keys("alexandr.bezpalyi@gmail.com")
-        password = drv.find_element_by_css_selector('input[name="password"]')
-        password.send_keys("Fhutjgnthbrc1!")
-        submit = drv.find_element_by_css_selector("button.sign-form__btn.button_with-loader")
-        submit.click()
-        drv.find_element_by_css_selector("div.drop-down__toggler.drop-down-toggler > button.navbar__profile-toggler")
-        yield drv
-        drv.quit()
-
     @pytest.mark.parametrize("url", links[:])
     def test_function(self, browser, url):
+        browser.get('https://stepik.org/catalog?language=en')
+        time.sleep(5)
+        sel = "a.navbar__auth.navbar__auth_login.st-link.st-link_style_button.ember-link.ember-view"
+        login = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, sel)))
+        login.click()
+
+        nam = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="login"]')))
+        nam.send_keys("alexandr.bezpalyi@gmail.com")
+        password = browser.find_element_by_css_selector('input[name="password"]')
+        password.send_keys("Fhutjgnthbrc1!")
+        submit = browser.find_element_by_css_selector("button.sign-form__btn.button_with-loader")
+        submit.click()
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                         "div.drop-down__toggler.drop-down-toggler > button.navbar__profile-toggler")))
         browser.get(url)
         answer = math.log(int(time.time()))
-        text_area = browser.find_element_by_css_selector("textarea")
+        text_area = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea")))
         text_area.send_keys(str(answer))
 
         submit = browser.find_element_by_css_selector("button.submit-submission")
         submit.click()
-        result = browser.find_element_by_css_selector("div.attempt__message > div > pre.smart-hints__hint")
+        result = WebDriverWait(browser, 100).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.attempt__message > div > pre.smart-hints__hint")))
         assert result.text == "Correct!"
-
-
-
