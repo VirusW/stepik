61005df4b7badca51d604903c3d73a2b34524f89
alexandr.bezpalyi@gmail.com <alexandr.bezpalyi@gmail.com>
2020-09-20 13:03:56 +0300

    add SolveAgain feature

--- test_3-6.py
+++ test_3-6.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -35,6 +35,11 @@ class TestLesson:
         navbar = "div.drop-down__toggler.drop-down-toggler > button.navbar__profile-toggler"
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, navbar)))
         browser.get(url)
+        try:
+            solve_again = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.again-btn.white")))
+            solve_again.click()
+        except TimeoutException:
+            pass
         answer = math.log(int(time.time()))
         text_area = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea")))
         text_area.send_keys(str(answer))
