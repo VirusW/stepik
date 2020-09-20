d21c63b710f48502a591aa250de453f216db7eea
alexandr.bezpalyi@gmail.com <alexandr.bezpalyi@gmail.com>
2020-09-20 13:03:18 +0300

    update login part

--- test_3-6.py
+++ test_3-6.py
@@ -23,20 +23,17 @@ class TestLesson:
 
     @pytest.mark.parametrize("url", links[:])
     def test_function(self, browser, url):
-        browser.get('https://stepik.org/catalog?language=en')
-        time.sleep(5)
-        sel = "a.navbar__auth.navbar__auth_login.st-link.st-link_style_button.ember-link.ember-view"
-        login = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, sel)))
-        login.click()
+        browser.get("https://stepik.org/catalog?auth=login&language=en")
 
-        nam = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="login"]')))
+        nam = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="login"]')))
         nam.send_keys("alexandr.bezpalyi@gmail.com")
         password = browser.find_element_by_css_selector('input[name="password"]')
         password.send_keys("Fhutjgnthbrc1!")
         submit = browser.find_element_by_css_selector("button.sign-form__btn.button_with-loader")
         submit.click()
-        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
-                                                                         "div.drop-down__toggler.drop-down-toggler > button.navbar__profile-toggler")))
+
+        navbar = "div.drop-down__toggler.drop-down-toggler > button.navbar__profile-toggler"
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, navbar)))
         browser.get(url)
         answer = math.log(int(time.time()))
         text_area = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea")))
