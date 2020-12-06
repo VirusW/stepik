0bebe5bde4f987eebb74309098480c90435bb52d
alexandr.bezpalyi@gmail.com <alexandr.bezpalyi@gmail.com>
2020-12-06 11:04:53 +0200

    task 3.6, last one

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_button_add_to_basket_exists(browser):
+    wait = WebDriverWait(browser, 10)
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket")))
+    assert button.tag_name == "button"
+    assert button.get_attribute("type") == "submit"
